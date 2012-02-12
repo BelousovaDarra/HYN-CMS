6b09b5dfed1a9badcaab9612cee92a00e9545677
Daniel Klabbers <u@hyn.me>
2012-02-13 00:35:54 +0100

    trying to fix remotes

---
+++ lib/hyn/bootstraps/twitter/main.lib.php
@@ -0,0 +1,21 @@
+<?PHP
+if(!defined("HYN")) { exit; }
+
+DOM::set_css( __DIR__ . DS . "lib" . DS . "docs" . DS . "assets" . DS . "css" . DS . "bootstrap.css" );
+
+DOM::set_css( __DIR__ . DS . "overrideimg.css" );
+
+DOM::set_js( HYN_PATH_PUBLIC_JS . "libs" . DS . "jq". DS . "jq.js" );
+
+DOM::set_js( __DIR__ . DS . "lib" . DS . "js" . DS . "bootstrap-alert.js" );
+DOM::set_js( __DIR__ . DS . "lib" . DS . "js" . DS . "bootstrap-button.js" );
+DOM::set_js( __DIR__ . DS . "lib" . DS . "js" . DS . "bootstrap-carousel.js" );
+DOM::set_js( __DIR__ . DS . "lib" . DS . "js" . DS . "bootstrap-collapse.js" );
+DOM::set_js( __DIR__ . DS . "lib" . DS . "js" . DS . "bootstrap-dropdown.js" );
+DOM::set_js( __DIR__ . DS . "lib" . DS . "js" . DS . "bootstrap-modal.js" );
+DOM::set_js( __DIR__ . DS . "lib" . DS . "js" . DS . "bootstrap-tooltip.js" );
+DOM::set_js( __DIR__ . DS . "lib" . DS . "js" . DS . "bootstrap-popover.js" );
+DOM::set_js( __DIR__ . DS . "lib" . DS . "js" . DS . "bootstrap-scrollspy.js" );
+DOM::set_js( __DIR__ . DS . "lib" . DS . "js" . DS . "bootstrap-tab.js" );
+DOM::set_js( __DIR__ . DS . "lib" . DS . "js" . DS . "bootstrap-transition.js" );
+DOM::set_js( __DIR__ . DS . "lib" . DS . "js" . DS . "bootstrap-typeahead.js" );
